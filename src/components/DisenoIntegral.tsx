c0db7c3ecacbb3ea7327cd43f9637734161532a3
Fernanda Olmedo <tu-correo@loquesea.com>
2026-09-07 11:32:53 -0500

    Portafolio de Servicios V.1.

--- src/components/DisenoIntegral.tsx
+++ src/components/DisenoIntegral.tsx
@@ -26,7 +26,7 @@ export default function DisenoIntegral() {
         <h2 className="about-title">Diseño Integral</h2>
         <p className="about-text">
           
-          El servicio de Diseño Integral está pensando en clientes que prefieren llevar a cabo todo el proceso del proyecto con nosotros, evaluamos desde el terreno que más te conviene, presupuesto, concepto, diseño, planos, trámites, elección de constructor o contratistas y supervisión de obra hasta concretar tu sueño. <br></br>
+          El servicio de Diseño Integral está pensando en clientes que prefieren llevar a cabo todo el proceso del proyecto con nosotros. Evaluamos desde el terreno que más te conviene, presupuesto, concepto, diseño, planos, trámites, elección de constructor o contratistas y supervisión de obra hasta concretar tus objetivos. <br></br>
           Ésto con la finalidad de que puedas obtener el mejor resultado de tu inversión. <br></br>
           Avanzamos juntos alineados a tu propósito y tiempo.<br></br>
         </p>
